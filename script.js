7410d2b75d387872cc08eb315d8d6f4705e88928
Kenneth Aidan Lee <158255466+kennethalee@users.noreply.github.com>
2025-01-09 16:39:36 +0900

    feat: add functionality for buttons

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 let firstOperand = "";
 let secondOperand = "";
 let currentOperator = "";
+let currentOperand = "";
+
+const screen = document.querySelector("#display-screen");
+const operands = document.querySelectorAll(".operand-btn");
+const operators = document.querySelectorAll(".operator-btn");
+const clearBtn = document.querySelector(".clear-btn");
+const deleteBtn = document.querySelector(".delete-btn");
+
+operands.forEach((btn) => {
+  btn.addEventListener("click", () => displayOperand(btn.dataset.operand));
+});
+
+operators.forEach((btn) => {
+  btn.addEventListener("click", () => displayOperand(btn.dataset.operator));
+});
+
+clearBtn.addEventListener("click", () => {
+  screen.textContent = "";
+  currentOperand = "";
+});
+
+deleteBtn.addEventListener("click", () => {
+  screen.textContent = screen.textContent.slice(0, -1);
+  currentOperand = screen.textContent;
+});
+
+function displayOperand(value) {
+  currentOperand += value;
+  screen.textContent = currentOperand;
+}
 
 function add(a, b) {
   return a + b;
